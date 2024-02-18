902f06c4ccd2944ec2ad918902106af3b4cdcbe4
kelkrsru <kelkrsru@gmail.com>
2024-02-18 14:27:54 +0700

    Комфортная и максимальная вместимость на карточках

--- kedrlog/mainpage/views.py
+++ kedrlog/mainpage/views.py
@@ -16,11 +16,11 @@ def index(request):
     content_block_yandex_map = ContentBlockYandexMap.objects.get(active=True)
     content_block_booking = ContentBlockBooking.objects.get(active=True)
 
-    min_cost = dict()
-    min_time = dict()
+    min_cost, min_time, include_guests = (dict() for _ in range(3))
     for house in content_block_booking.houseshowbooking_set.all():
         if house.house.rate_house.filter(active=True).exists():
             min_cost[house.house.pk] = round(house.house.rate_house.get(active=True).price)
+            include_guests[house.house.pk] = house.house.rate_house.get(active=True).guests_in_price
             min_time[house.house.pk] = house.house.rate_house.get(active=True).min_time
 
     context = {
@@ -31,6 +31,7 @@ def index(request):
         'content_block_yandex_map': content_block_yandex_map,
         'content_block_booking': content_block_booking,
         'min_cost': min_cost,
-        'min_time': min_time
+        'min_time': min_time,
+        'include_guests': include_guests,
     }
     return render(request, template, context)
